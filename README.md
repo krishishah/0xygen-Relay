f514e4bc2af9fcd484d6c2aeaa94e34bb21dd47a
Krishi Shah <krishikshah@gmail.com>
2017-12-29 00:21:52 +0300

    added ethereum and 0x dependencies

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # 0x-relay
 Decentralised Ethereum Token Relay
+
+
+Credit to this tutorial: http://mherman.org/blog/2016/11/05/developing-a-restful-api-with-node-and-typescript/#.WkONI7SZ3OS for boilerplate setup
---
+++ src/modules/rest-api/index.ts
@@ -0,0 +1,83 @@
+import {Router, Request, Response, NextFunction} from 'express';
+
+export class V0RestApiRouter {
+  router: Router
+
+  /**
+   * Initialize the HeroRouter
+   */
+  constructor() {
+    this.router = Router();
+    this.init();
+  }
+
+  /**
+   * GET token pairs.
+   */
+  public getTokenPairs(req: Request, res: Response, next: NextFunction) {
+    res.send(Heroes);
+  }
+
+  /**
+   * GET orderbook.
+   */
+  public getOrderBook(req: Request, res: Response, next: NextFunction) {
+    res.send(Heroes);
+  }
+
+  /**
+   * GET orders.
+   */
+  public getOrders(req: Request, res: Response, next: NextFunction) {
+    res.send(Heroes);
+  }
+
+  /**
+   * GET order.
+   */
+  public getOrder(req: Request, res: Response, next: NextFunction) {
+    res.send(Heroes);
+  }
+
+  /**
+   * GET fees.
+   */
+  public getFees(req: Request, res: Response, next: NextFunction) {
+    res.send(Heroes);
+  }
+
+  /**
+   * POST order.
+   */
+  public postOrder(req: Request, res: Response, next: NextFunction) {
+    res.send(Heroes);
+  }
+
+  /**
+   * GET tokens.
+   */
+  public getTokens(req: Request, res: Response, next: NextFunction) {
+    res.send(Heroes);
+  }
+
+  /**
+   * Take each handler, and attach to one of the Express.Router's
+   * endpoints.
+   */
+  init() {
+    this.router.get('/token_pairs', this.getTokenPairs);
+    this.router.get('/orderbook', this.getOrderBook);
+    this.router.get('/orders', this.getOrder);
+    this.router.get('/order/:orderHash', this.getOrder);
+    this.router.get('/fees', this.getFees);
+    this.router.post('/order', this.postOrder);
+    this.router.get('/tokens', this.getTokens);
+  }
+
+}
+
+// Create the HeroRouter, and export its configured Express.Router
+const v0RestApiRoutes = new V0RestApiRouter();
+v0RestApiRoutes.init();
+
+export default v0RestApiRoutes.router;
